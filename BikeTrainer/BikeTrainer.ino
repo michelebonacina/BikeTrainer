7167984b71c7dc4a288acedd22c2ef7f5bd343aa
michelebonacina <michele.bonacina@gmail.com>
2018-02-16 20:10:56 +0100

    added calculated data
    total session distance, total session time, instant velocity, instant
    cadence rpm, average velocity, average cadence rpm

--- BikeTrainer/BikeTrainer.ino
+++ BikeTrainer/BikeTrainer.ino
@@ -1,8 +1,11 @@
 /**
  * aBikeTrainer - Arduino Bike Trainer
  * 
- * Read and analyze data from bike sensors.
- * Monitor wheel & cadence sensor and count revolutions. 
+ * Reads and analyzes data from bike sensors.
+ * Monitors wheel & cadence sensor and counts revolutions. 
+ * Calculates total time and total distace.
+ * Calculates instant velocity and instant cadence rpm 
+ * Calculates average velocity and average cadence rpm
  * 
  * Sends wheel & cadence counter values on serial port.
  * 
@@ -36,28 +39,35 @@ const int wheelSensorPort   = A0; // wheel sensor conneted on analog port 0
 const int cadenceSensorPort = A1; // cadence sensor conneted on analog port 1
 
 // sensors configuration
-const int sensorLowLevel  = 400;   // under this analog level the sensor in open
-const int sensorHighLevel = 600;   // over this analog level the sensor in closed
-const int latency         = 10;    // time in millis to wait between sensors state checks
+const int sensorLowLevel  = 400; // under this analog level the sensor in open
+const int sensorHighLevel = 600; // over this analog level the sensor in closed
+const int latency         = 10;  // time in millis to wait between sensors state checks
+const int printLatency    = 250; // time in millis to wait between data print updates
 
 // counters
-int wheelCounter      = 0;      // number of wheel turns
-int cadenceCounter    = 0;      // number of pedal turns
-boolean wheelState    = false;  // state of the wheel sensor - true closed, false open
-boolean cadenceState  = false;  // state of the cadence sensor - true closed, false open
+int wheelCounter                = 0;     // number of wheel turns
+int cadenceCounter              = 0;     // number of pedal turns
+boolean wheelState              = false; // state of the wheel sensor - true closed, false open
+boolean cadenceState            = false; // state of the cadence sensor - true closed, false open
+long wheelRevolutionDuration    = 0;     // time in millis for a wheel revolution
+long wheelRevolutionLastTime    = 0;     // last time the wheel sensor was closed
+long cadenceRevolutionDuration  = 0;     // time in millis for a cadence revolution
+long cadenceRevolutionLastTime  = 0;     // last time the cadence sensor was closed
 
 // data computing
-long startTime      = 0; // trainig starting time
+long startTime            = 0;    // trainig starting time
+long printTime            = 0;    // last data printing time
+float wheelCircumference  = 2096; // wheel circumference in centimeters
 
 
 /**
  * Main settings.
  */
 void setup() {
-  
   // initialize serial comunication  
   Serial.begin(9600);
-
+  // initialice computation variables
+  startTime = millis();
 }
 
 /*
@@ -81,6 +91,10 @@ void loop() {
       // before this, the sensor was open
       // the wheel makes another turn
       wheelCounter++;
+      // calculate time for one revolution
+      wheelRevolutionDuration = millis() - wheelRevolutionLastTime;
+      // set revolution starting time
+      wheelRevolutionLastTime = millis();
     }
     // sets the sensor closed
     wheelState = true;
@@ -97,16 +111,56 @@ void loop() {
       // before this, the sensor was open
       // the pedal makes another turn
       cadenceCounter++;
+      // calculate time for one revolution
+      cadenceRevolutionDuration = millis() - cadenceRevolutionLastTime;
+      // set revolution starting time
+      cadenceRevolutionLastTime = millis();
     }
     // sets the sensor closed
     cadenceState = true;
   }
+  
+  // compute session data
+  // total time in this session in hour, minute, seconds
+  int totalTimeHour = (millis() - startTime) / 3600000;
+  int totalTimeMinute = ((millis() - startTime) % 3600000) / 60000;
+  int totalTimeSecond = ((millis() - startTime) % 60000) / 1000;
+  // total distance amount in this session in km
+  float totalDistance = wheelCircumference * wheelCounter / 100000.0;
+  // average velocity in km/h, based on distance and total session time
+  float averageVelocity = totalDistance / ((millis() - startTime) / 3600000.0);
+  // avrage cadence rpm, based on cadence and total session time
+  int averageCadenceRpm = (int) (cadenceCounter / ((millis() - startTime) / 60000.0));
+  // instant velocity, based on one wheel revolution
+  float instantVelocity = (wheelCircumference / 100000.0) / (wheelRevolutionDuration / 3600000.0);
+  // instant cadence rpm, based on one pedal revolution
+  int instantCadenceRpm = (int) (1.0 / (cadenceRevolutionDuration / 60000.0));
 
   // send sensor state
-  Serial.print(wheelCounter);
-  Serial.print("  ");
-  Serial.println(cadenceCounter);
-
+  if (millis() - printTime > printLatency) {
+    Serial.print(wheelCounter);
+    Serial.print("  ");
+    Serial.print(cadenceCounter);
+    Serial.print("  ");
+    Serial.print(totalTimeHour);
+    Serial.print(":");
+    Serial.print(totalTimeMinute);
+    Serial.print(":");
+    Serial.print(totalTimeSecond);
+    Serial.print("  ");
+    Serial.print(totalDistance);
+    Serial.print("  ");
+    Serial.print(instantVelocity);
+    Serial.print("  ");
+    Serial.print(instantCadenceRpm);
+    Serial.print("  ");
+    Serial.print("  ");
+    Serial.print(averageVelocity);
+    Serial.print("  ");
+    Serial.print(averageCadenceRpm);
+    Serial.println("  ");
+  }
+  
   // wait before next read
   delay(latency);
 
